26cef21551ef2b51a9f84527c050efb394c598f5
hvthhien <hvthhien@gmail.com>
2022-02-08 00:06:06 +0700

    update ios libary

--- ios/Classes/SwiftLibaukDartPlugin.swift
+++ ios/Classes/SwiftLibaukDartPlugin.swift
@@ -3,12 +3,9 @@ import UIKit
 
 public class SwiftLibaukDartPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "libauk_dart", binaryMessenger: registrar.messenger())
-    let instance = SwiftLibaukDartPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    //TODO: Will be handled inside the app
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
   }
 }
